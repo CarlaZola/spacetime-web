3e38a2e57bce8d6ecc3e48041a224b30a3190b9c
carla <carla.zollaa@gmail.com>
2023-05-16 16:02:42 -0300

    feat: initial settings

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <h1>Hello World!</h1>
+}
+
+// tudo que estiver dentro da pasta app com o nome 'page.tsx' será automaticamente convertido em uma rota da aplicação
