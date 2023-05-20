e761a0a3deeb2f51be52df01b04e24c252b6ee60
carla <carla.zollaa@gmail.com>
2023-05-20 12:01:10 -0300

    feat: components and oauth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+}
+
+// next não carrega imagens externas de qualquer endereço, é necessário passar quais os domínios serão utilizados para obter as imagens
 
 module.exports = nextConfig
